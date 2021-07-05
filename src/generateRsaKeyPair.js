f32bcb28527bde68ad234caf44b54bb957e39deb
BrunoJohann <bruno.johann@hotmail.com>
2021-07-05 15:21:59 -0300

    Refactory generateRsaKeyPair

--- src/generateRsaKeyPair.js
+++ src/generateRsaKeyPair.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
 
-const rsaGenerateKeyPair = promisify(forge.pki.rsa.generateKeyPair);
+const generateKeyPair = promisify(forge.pki.rsa.generateKeyPair);
 
 const generateRsaKeyPair = async () => {
-    const { publicKey, privateKey } = await rsaGenerateKeyPair({bits: 2048, workers: 2});
+    const { publicKey, privateKey } = await generateKeyPair({bits: 2048, workers: 2});
     const pathFilePublicKey = path.resolve(__dirname, 'keys', 'public.pem');
     const pathFilePrivateKey = path.resolve(__dirname, 'keys', 'private.pem');
     fs.writeFileSync(pathFilePublicKey, forge.pki.publicKeyToPem(publicKey));
